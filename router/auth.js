ee21104066372f22205544d5e82b6eadfe5a67c0
Achyut <aoogleak@gmail.com>
2022-12-19 00:36:07 +0545

    1.5 backend testing completed

--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const router = express.Router();
+
+//Mailing dependencies
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const Oauth2 = google.auth.OAuth2;
+
+//Nodemailer environemnts
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const sender = process.env.SENDER_ID;
+const refreshToken = process.env.REFRESH_TOKEN;
+const Redirect_URL = process.env.REDIRECT_URL;
+
+//Generate Access token with refresh tooken
+const OAuth2Client = new google.auth.OAuth2(
+	clientId,
+	clientSecret,
+	Redirect_URL
+);
+OAuth2Client.setCredentials({ refresh_token: refreshToken });
 
 router.get("/", (req, res) => {
 	res.render("home");
@@ -8,7 +27,7 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
 	//Grab user input and send message with Html feature added
-	const { fromEmail, toEmail, title, body } = req.body;
+	const { fromEmail, user, toEmail, title, body } = req.body;
 	const output = `
         <h4>Mailing Message Service</h4>
         <h2>${title}</h2>
@@ -21,23 +40,27 @@ router.post("/", (req, res) => {
     `;
 
 	async function sendMail() {
-		let testAccount = await nodemailer.createTestAccount();
+		//let testAccount = await nodemailer.createTestAccount();
 		try {
-			//const accessToken = await OAuth2Client.getAccessToken();
+			const accessToken = await OAuth2Client.getAccessToken();
 
 			const transport = nodemailer.createTransport({
-				host: "smtp.ethereal.email",
+				host: "smtp.gmail.com",
 				port: "587",
 				secure: false,
 				service: "mail",
 				auth: {
-					user: testAccount.user, // generated ethereal user
-					pass: testAccount.pass, // generated ethereal password
+					type: "OAuth2",
+					user: sender,
+					clientId: clientId,
+					clientSecret: clientSecret,
+					refreshToken: refreshToken,
+					accessToken: accessToken,
 				},
 			});
 
 			const mailOptions = {
-				from: `CoolGuy <${fromEmail}>`,
+				from: `${user} <${fromEmail}>`,
 				to: toEmail,
 				subject: title,
 				html: output,
